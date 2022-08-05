935914cdb9b92814bc18dfda9d2ab030fe454ef1
Vanessa Vun <vanessavun@gmail.com>
2022-08-05 12:18:21 -0700

    Heroku attempt

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const db = knex({
     client: 'pg',
     connection: {
       host : '127.0.0.1',
-      port: 5432,
       user : 'postgres',
       password : 'test',
       database : 'smart-brain'
@@ -31,9 +30,8 @@ app.use(bodyParser.json());
 app.use(cors());
 
 //ROOT
-app.get('/', (req, res) => {
-    res.send("Success")
-})
+app.get('/', (req, res) => { res.send("Success") })
+
 //SIGNIN (post, pw over HTTP body)
 app.post('/signin', (req, res) => { signin.handleSignIn(req, res, db, bcrypt) });
 
@@ -49,7 +47,7 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) });
 //IMAGEURL (post, handle Face Recognition API from backend)
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
 
-app.listen(3000, () => {
-    console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running on port ${process.env.PORT}`);
 })
 
