caf5eb93ab4db78c4a9f2bbe8cebd3a6f5d9f90f
Vanessa Vun <vanessavun@gmail.com>
2022-12-02 18:22:35 -0800

    Edited Render config

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const db = knex({
   client: 'pg',
   connection: {
     connectionString : process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
+    ssl: { rejectUnauthorized: false },
+    host: 'dpg-ce59noun6mpk2bj7tt1g-a',
+    port: 5432,
+    user: 'face_recognition_database_q1rl_user',
+    password: process.env.DATABASE_PW,
+    database: 'face_recognition_database_q1rl'
   }
 });
 /* my local host
